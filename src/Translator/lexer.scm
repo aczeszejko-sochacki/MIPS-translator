deed1d99f14db721ae8130e785f82322e9a7f657
aleksander <aleksanderczeso@gmail.com>
2018-05-19 00:29:08 +0200

    Fixed writing to file. Lexer file created.

---
+++ src/Translator/lexer.scm
@@ -0,0 +1,2 @@
+(define (lexer line)
+  line)
--- src/Translator/translator.scm
+++ src/Translator/translator.scm
@@ -1,10 +1,13 @@
+;;; Imports
+(load "src/Translator/lexer.scm")
+
 ;;; Main function. Translates MIPS code to Asembler code.
 ;;; This is simulation of an object
 (define (translator input-file output-file)
 
   ;;; Variables with content of input and output file
   (define input-file-content (list))
-  (define output-file-content "")
+  (define output-file-content (list))
   ;;; I/O
   (define in (open-input-file input-file))
   (define out (open-output-file output-file))
@@ -16,21 +19,28 @@
     (if (eof-object? new-line)
       (close-input-port	in)
       ((begin
-        (set! input-file-content (
-          append! input-file-content (list new-line)))
+        (set! input-file-content 
+        (append! input-file-content (list new-line)))
         read-file))))
 
   ;;; Translates MIPS code
-  (define (translate-file)
-    ;; To do
-    (set! output-file-content input-file-content))
+  (define (translate-file)         
+    (if (null? input-file-content)
+      (error "Translating empty file")  ; Raise exception
+      (set! output-file-content 
+        (map lexer input-file-content))))  ; Lexer line by line
 
   ;;; Writes translated code to file
   (define (write-to-file)
-  (begin
-    (display ".text" out)
-    (display output-file-content out)			
-    (close-output-port out)))
+    (define (write-line line)
+      (display line out)
+      (newline out))
+
+    (begin
+      (display ".text" out)
+      (newline out)
+      (map write-line output-file-content)			
+      (close-output-port out)))
 
   (define (dispatch message)
     (cond ((eqv? message 'read-file) read-file)
