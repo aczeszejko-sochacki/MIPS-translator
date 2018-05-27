abe5ceb45291d1086b731b5129ed3da8b05ea3e4
aleksander <aleksanderczeso@gmail.com>
2018-05-27 15:11:52 +0200

    Implemented decoding to hexadecimal

--- src/LineTranslators/Codes/hexadecimal_code.scm
+++ src/LineTranslators/Codes/hexadecimal_code.scm
@@ -1,5 +1,18 @@
 ;;; Decode bit-string to hexadecimal
 (define (decode-binary command)
-  command)
-  
 
+  ;;; Decoded to hexadecimal
+  (define decoded-hex 
+    (number->string 
+      (bit-string->signed-integer command) 
+      16))
+
+  ;;; Need to return value on 8 digits
+  (define additional-zeros
+    (make-string 
+      (- 8 (string-length decoded-hex)) 
+      #\0))
+
+  ;; Return final version of decoded command
+  (string-upcase 
+    (string-append additional-zeros decoded-hex)))
